3ebe40c4c7ea1fdab6f25800b9d2e21189c35a70
Jether Omictin <jethomictin@gmail.com>
2024-02-21 21:59:12 +0800

    landing page

---
+++ script.js
@@ -0,0 +1,25 @@
+TweenMax.staggerFrom(".form-group", 1, {
+    delay: 0.2,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+  }, 0.2);
+  
+  TweenMax.staggerFrom(".contact-info-container > *", 1, {
+    delay: 0,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+  }, 0.1);
+  
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    
+    TweenMax.staggerFrom(".about-us-info-container > *", 1, {
+        delay: 0,
+        opacity: 0,
+        y: 20,
+        ease: Expo.easeInOut
+    }, 0.1);
+  });
+  
