ada799dab49715172f33aed885507d667737442a
Jether Omictin <jethomictin@gmail.com>
2024-03-08 15:09:16 +0800

    post request

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
-TweenMax.staggerFrom(".form-group", 1, {
+const logo = document.getElementById("img-logo");
+
+logo.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
+
+TweenMax.staggerFrom(
+  ".form-group",
+  1,
+  {
     delay: 0.2,
     opacity: 0,
     y: 20,
-    ease: Expo.easeInOut
-  }, 0.2);
-  
-  TweenMax.staggerFrom(".contact-info-container > *", 1, {
+    ease: Expo.easeInOut,
+  },
+  0.2
+);
+
+TweenMax.staggerFrom(
+  ".contact-info-container > *",
+  1,
+  {
     delay: 0,
     opacity: 0,
     y: 20,
-    ease: Expo.easeInOut
-  }, 0.1);
-  
-  
-  document.addEventListener("DOMContentLoaded", function() {
-    
-    TweenMax.staggerFrom(".about-us-info-container > *", 1, {
-        delay: 0,
-        opacity: 0,
-        y: 20,
-        ease: Expo.easeInOut
-    }, 0.1);
-  });
-  
+    ease: Expo.easeInOut,
+  },
+  0.1
+);
+
+document.addEventListener("DOMContentLoaded", function () {
+  TweenMax.staggerFrom(
+    ".about-us-info-container > *",
+    1,
+    {
+      delay: 0,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut,
+    },
+    0.1
+  );
+});
